8fdd00e5c5d7a17302f6ae870961b643eab7e05c
hyacinta <kimkyuree84@gmail.com>
2020-11-16 11:01:49 +0900

    btn_top css & js

--- js/common.js
+++ js/common.js
@@ -1,7 +1,17 @@
 // DOM
 const $nav = document.querySelector(".nav");
+const $btnTop = document.querySelector(".btn_top");
+const $logo = document.querySelector(".logo a");
 
 // EVENT
 window.onscroll = () => {
   $nav.classList.toggle('scroll', window.scrollY > 80);
+  $btnTop.style.bottom = window.scrollY > 500 ? '40px': '30px';
+  $btnTop.style.opacity = window.scrollY > 500 ? '1': '0';
 }
+$btnTop.onclick = () => {
+  window.scrollTo(0, 0);
+};
+$logo.onclick = () => {
+  window.scrollTo(0, 0);
+};
