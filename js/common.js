4659b78dea2fb1dc1107dc83cf430345846fdf89
hyacinta <kimkyuree84@gmail.com>
2020-11-17 23:00:20 +0900

    detail modify & jejuMap modify

--- js/common.js
+++ js/common.js
@@ -2,6 +2,13 @@
 const $nav = document.querySelector('.nav');
 const $btnTop = document.querySelector('.btn_top');
 const $logo = document.querySelector('.logo');
+const $teaserIframe = document.querySelector('.teaser iframe');
+const $terroir1 = document.querySelector('.terroir1');
+const $terroir2 = document.querySelector('.terroir2');
+const $terroir3 = document.querySelector('.terroir3');
+const $mapDot1 = document.querySelector('.jejuMap .dot1');
+const $mapDot2 = document.querySelector('.jejuMap .dot2');
+const $mapDot3 = document.querySelector('.jejuMap .dot3');
 
 // EVENT
 window.onscroll = () => {
@@ -11,3 +18,28 @@ window.onscroll = () => {
 }
 $btnTop.onclick = () => window.scrollTo(0, 0);
 $logo.onclick = () => window.scrollTo(0, 0);
+
+$terroir1.onmouseover = ({ target }) => {
+  if (!target.matches('.terroir1 h3 span')) return;
+  $mapDot1.classList.add('active');
+}
+$terroir2.onmouseover = ({ target }) => {
+  if (!target.matches('.terroir2 h3 span')) return;
+  $mapDot2.classList.add('active');
+}
+$terroir3.onmouseover = ({ target }) => {
+  if (!target.matches('.terroir3 h3 span')) return;
+  $mapDot3.classList.add('active');
+}
+$terroir1.onmouseout = ({ target }) => {
+  if (!target.matches('.terroir1 h3 span')) return;
+  $mapDot1.classList.remove('active');
+}
+$terroir2.onmouseout = ({ target }) => {
+  if (!target.matches('.terroir2 h3 span')) return;
+  $mapDot2.classList.remove('active');
+}
+$terroir3.onmouseout = ({ target }) => {
+  if (!target.matches('.terroir3 h3 span')) return;
+  $mapDot3.classList.remove('active');
+}
